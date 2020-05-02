58e9c6e2a80cc26f6f27112a34a656fd6cc86ddd
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2020-05-02 09:46:22 -0700

    initial setup with fresh Park component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Park from './components/Park'
+
+function App() {
+  return (
+    <div className="App">
+      <Park />
+      <Park />
+      <Park />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Park.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Park() {
+  return (
+    <div className="Park">
+      <h1> I'm a Park! </h1>
+    </div>
+  );
+}
+
+export default Park;
